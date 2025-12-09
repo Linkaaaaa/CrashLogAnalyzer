d7bf3c97b286c757c2bd66a5975c657c633c977b
Linkaaaaa <christian.pagliarani@gmail.com>
2025-12-09 16:22:36 +0100

    Make lists IReadOnly

--- CrashLogAnalyzer/Interferences.cs
+++ CrashLogAnalyzer/Interferences.cs
@@ -5,19 +5,19 @@
 /// </summary>
 public static class Interference
 {
-    private static readonly List<string> AsusGPUTweakIII = ["glew32.dll", "GTIII-OSD64-GL.dll", "GTIII-OSD64-VK.dll", "GTIII-OSD64.dll"];
-    private static readonly List<string> BitDefender = ["bdhkm64.dll", "atcuf64.dll"];
-    private static readonly List<string> Groupy = ["groupy_64.dll"];
-    private static readonly List<string> Medal = ["medal-hook64.dll"];
-    private static readonly List<string> NvidiaPhotoModeGameFilter = ["NvCamera64.dll"];
-    private static readonly List<string> NvidiaSmoothMotion = ["nvcuda64.dll", "nvdxgdmal64.dll", "NvPresent64.dll"];
-    private static readonly List<string> Overwolf = ["OWClient.dll", "OWExplorer.dll", "OWUtils.dll"];
-    private static readonly List<string> RivaTurnerStatisticsServer = ["RTSSHooks64.dll"];
+    private static readonly IReadOnlyList<string> AsusGPUTweakIII = ["glew32.dll", "GTIII-OSD64-GL.dll", "GTIII-OSD64-VK.dll", "GTIII-OSD64.dll"];
+    private static readonly IReadOnlyList<string> BitDefender = ["bdhkm64.dll", "atcuf64.dll"];
+    private static readonly IReadOnlyList<string> Groupy = ["groupy_64.dll"];
+    private static readonly IReadOnlyList<string> Medal = ["medal-hook64.dll"];
+    private static readonly IReadOnlyList<string> NvidiaPhotoModeGameFilter = ["NvCamera64.dll"];
+    private static readonly IReadOnlyList<string> NvidiaSmoothMotion = ["nvcuda64.dll", "nvdxgdmal64.dll", "NvPresent64.dll"];
+    private static readonly IReadOnlyList<string> Overwolf = ["OWClient.dll", "OWExplorer.dll", "OWUtils.dll"];
+    private static readonly IReadOnlyList<string> RivaTurnerStatisticsServer = ["RTSSHooks64.dll"];
 
     /// <summary>
     /// Grouped dll groups by software names.
     /// </summary>
-    public static readonly Dictionary<string, IEnumerable<string>> DllGroups = new()
+    public static readonly Dictionary<string, IReadOnlyList<string>> DllGroups = new()
     {
         { "Asus GPU Tweak III", AsusGPUTweakIII },
         { "Bit Defender", BitDefender },
